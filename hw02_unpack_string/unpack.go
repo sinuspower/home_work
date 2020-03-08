439155ec674d45a5795b26a9781527cfb981c364
Oleg Kovalev <SinusPower@gmail.com>
2020-03-08 21:45:11 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,13 +2,52 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	var result string
-	result = _
+// Unpack makes basic unpacking of given string.
+// This function allows skipping characters by '\'.
+func Unpack(in string) (string, error) {
+	if in == "" {
+		return "", nil
+	}
 
-	return result, nil
+	var b strings.Builder
+	runes := []rune(in)
+	n := len(runes)
+	escape := false
+
+	for i := 0; i < n-1; i++ {
+		if unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i+1]) && !escape { //numbers isn't allowed
+			return "", ErrInvalidString
+		}
+		if runes[i] == runes[i+1] && runes[i] != '\\' && !escape { // duplicated chars isn't allowed, except `\`
+			return "", ErrInvalidString
+		}
+		if escape && unicode.IsDigit(runes[i]) && !unicode.IsDigit(runes[i+1]) { // write single digit
+			b.WriteRune(runes[i])
+		}
+		if escape = runes[i] == '\\' && !escape; escape {
+			continue
+		}
+		if unicode.IsDigit(runes[i+1]) {
+			count, _ := strconv.Atoi(string(runes[i+1]))
+			if count != 0 {
+				b.WriteString(strings.Repeat(string(runes[i]), count))
+			}
+		} else if !unicode.IsDigit(runes[i]) { // write single char
+			b.WriteRune(runes[i])
+		}
+	}
+	if escape { // write last
+		b.WriteRune(runes[n-1])
+	}
+	if !unicode.IsDigit(runes[n-1]) && runes[n-1] != '\\' { // write last
+		b.WriteRune(runes[n-1])
+	}
+	return b.String(), nil
 }
