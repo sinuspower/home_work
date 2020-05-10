7a695fa4cb4b53919f75807dfa00458d5a4003d2
Oleg Kovalev <SinusPower@gmail.com>
2020-05-11 02:08:51 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,120 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/udhos/equalfile"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Run("file not found", func(t *testing.T) {
+		err := Copy("test", "testCopy", 0, 0)
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("zero offset and limit", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		outPath := "testdata/output.txt"
+
+		err := Copy(inPath, outPath, 0, 0)
+		require.Nil(t, err, "error copying")
+
+		cmp := equalfile.New(nil, equalfile.Options{}) // compare using single mode
+		filesEqual, err := cmp.CompareFile(inPath, outPath)
+
+		require.Nil(t, err, "error comparing files")
+		require.True(t, filesEqual, "files are not equal")
+
+		err = os.Remove(outPath)
+		require.Nil(t, err, "error removing output file")
+	})
+
+	t.Run("limit greather than file size", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		inFile, err := os.Open(inPath)
+		require.Nil(t, err, "error open input file")
+		inFileInfo, err := inFile.Stat()
+		require.Nil(t, err, "error getting input file info")
+		inSize := inFileInfo.Size()
+		inFile.Close()
+
+		outPath := "testdata/output.txt"
+		err = Copy(inPath, outPath, 0, inSize+10)
+		require.Nil(t, err, "error copying")
+
+		cmp := equalfile.New(nil, equalfile.Options{})
+		filesEqual, err := cmp.CompareFile(inPath, outPath)
+
+		require.Nil(t, err, "error comparing files")
+		require.True(t, filesEqual, "files are not equal")
+
+		err = os.Remove(outPath)
+		require.Nil(t, err, "error removing output file")
+	})
+
+	t.Run("offset greather than file size", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		inFile, err := os.Open(inPath)
+		require.Nil(t, err, "error open input file")
+		inFileInfo, err := inFile.Stat()
+		require.Nil(t, err, "error getting input file info")
+		inSize := inFileInfo.Size()
+		inFile.Close()
+
+		outPath := "testdata/output.txt"
+		err = Copy(inPath, outPath, inSize+10, 0)
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("limit less than zero", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		outPath := "testdata/output.txt"
+
+		err := Copy(inPath, outPath, 0, -1000000)
+		require.Equal(t, err, ErrNegativeParameter)
+	})
+
+	t.Run("offset less than zero", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		outPath := "testdata/output.txt"
+
+		err := Copy(inPath, outPath, -14, 0)
+		require.Equal(t, err, ErrNegativeParameter)
+	})
+
+	t.Run("both offset and limit less than zero case", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		outPath := "testdata/output.txt"
+
+		err := Copy(inPath, outPath, -14, -87)
+		require.Equal(t, err, ErrNegativeParameter)
+	})
+
+	t.Run("offset equals file size", func(t *testing.T) {
+		inPath := "testdata/input.txt"
+		inFile, err := os.Open(inPath)
+		require.Nil(t, err, "error open input file")
+		inFileInfo, err := inFile.Stat()
+		require.Nil(t, err, "error getting input file info")
+		inSize := inFileInfo.Size()
+		inFile.Close()
+
+		outPath := "testdata/output.txt"
+		err = Copy(inPath, outPath, inSize, 10)
+		require.Nil(t, err)
+
+		outFile, err := os.Open(outPath)
+		require.Nil(t, err, "error open output file")
+		outFileInfo, err := outFile.Stat()
+		require.Nil(t, err, "error getting output file info")
+		outSize := outFileInfo.Size()
+		outFile.Close()
+
+		require.Equal(t, int(outSize), 0, "output file size greather than zero")
+
+		err = os.Remove(outPath)
+		require.Nil(t, err, "error removing output file")
+	})
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,9 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/sinuspower/home_work/hw07_file_copying
 
 go 1.14
+
+require (
+	github.com/cheggaaa/pb/v3 v3.0.4
+	github.com/stretchr/testify v1.5.1
+	github.com/udhos/equalfile v0.3.0
+)
