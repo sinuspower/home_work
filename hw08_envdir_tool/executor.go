9613694c0582e0ec6acefb44b0154d01abee3199
Oleg Kovalev <SinusPower@gmail.com>
2020-05-14 01:05:59 +0300

    Fixed : Top level comment should end in a period (godot)

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 )
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	if len(cmd) == 0 {
 		return
