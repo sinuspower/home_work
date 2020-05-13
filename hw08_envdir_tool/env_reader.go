0819a2085ddcdcacecaa181163c7c9b66d5f3475
Oleg Kovalev <SinusPower@gmail.com>
2020-05-14 00:47:53 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,76 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]string
+
+var (
+	ErrCanNotOpenDir  = errors.New("can not open directory")
+	ErrWrongFileName  = errors.New("wrong file name")
+	ErrCanNotOpenFile = errors.New("can not open file")
+	ErrCanNotReadLine = errors.New("can not read line from file")
+)
+
+func readString(f *os.File) (string, error) {
+	fInfo, err := f.Stat()
+	if err != nil {
+		return "", err
+	}
+	if fInfo.Size() == 0 {
+		return "", nil
+	}
+	reader := bufio.NewReader(f)
+	result := ""
+	isPrefix := true
+	for isPrefix {
+		line, p, err := reader.ReadLine()
+		if err != nil {
+			return "", err
+		}
+		result += string(bytes.ReplaceAll(line, []byte{0}, []byte("\n")))
+		isPrefix = p
+	}
+	return result, nil
+}
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, ErrCanNotOpenDir
+	}
+
+	env := make(Environment)
+	for _, file := range files {
+		if !file.IsDir() {
+			name := file.Name()
+			if strings.Contains(name, "=") {
+				return nil, ErrWrongFileName
+			}
+			f, err := os.Open(dir + string(os.PathSeparator) + name)
+			if err != nil {
+				return nil, ErrCanNotOpenFile
+			}
+			value, err := readString(f)
+			if err != nil {
+				return nil, ErrCanNotReadLine
+			}
+			env[name] = value
+			f.Close()
+		}
+	}
+
+	if len(env) == 0 {
+		env = nil
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
+	if len(cmd) == 0 {
+		return
+	}
+
+	for key, value := range env {
+		if value == "" {
+			os.Unsetenv(key)
+		} else {
+			_, ok := os.LookupEnv(key)
+			if ok {
+				os.Unsetenv(key)
+			}
+			os.Setenv(key, value)
+		}
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) // nolint:gosec
+	command.Env = os.Environ()
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			return exitError.ExitCode()
+		}
+		log.Fatal(err)
+	}
+
 	return
 }
